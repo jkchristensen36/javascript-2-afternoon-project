1e03609e529288a5cd056633ed6701e18715a883
Jordan Christensen <jchristensen@Jordans-MacBook-Pro.local>
2020-07-09 15:37:24 -0700

    initial commit

--- arrays/practice.js
+++ arrays/practice.js
@@ -18,9 +18,9 @@ var arr = [10,20,30];
 */
 
 //Code Here
-
-
-
+function first(arr) {
+  return arr[0]
+};
 ////////// PROBLEM 2 //////////
 
 // Do not edit the code below.
@@ -33,9 +33,9 @@ var arr = [40,50,60];
 */
 
 //Code Here
-
-
-
+function last(arr) {
+  return arr[arr.length - 1]
+}
 ////////// PROBLEM 3 //////////
 
 // Do not edit the code below.
@@ -48,9 +48,11 @@ var family = ['Tyler', 'Jordyn', 'Ryan', 'Chelsey', 'Ireland'];
 */
 
 //Code Here
-
-
-
+function looper(family) {
+  for(let i = 0; i < family.length; i++) {
+    alert(family[i]);
+  }
+}
 ////////// PROBLEM 4 //////////
 
 // Do not edit the code below.
@@ -63,9 +65,11 @@ var letters = ['A', 'B', 'C', 'D', 'E'];
 */
 
 //Code Here
-
-
-
+function reversedLooper(letters) {
+  for(let i = letters.length - 1; i >= 0; i--) {
+    alert(letters[i]);
+  }
+}
 ////////// PROBLEM 5 //////////
 
 // Do not edit the code below.
@@ -78,13 +82,14 @@ var nums = [1,2,3,6,22,98,45,23,22,12];
 */
 
 //Code Here
-
-
-
-
-
-
-
+function evenFinder(nums) {
+  for (let i = 0; i < nums.length; i++){
+   if (nums[i] % 2 !== 0) {
+    nums.splice(i, 1);
+   }
+  }
+  return nums;
+}
 /////////////////////// EXTRA PRACTICE PROBLEMS BELOW ////////////////////
 ////////// MOVE ONTO NEXT SECTION BEFORE WORKING ON THESE ////////////////
 
